cd751d564c6135daff301a8b58eb491669129875
Ivan Ratushnjak <ratushnjak.ivanko@gmail.com>
2026-09-06 20:30:11 +0300

    add task solution

--- src/main.js
+++ src/main.js
@@ -1,1 +1,9 @@
 'use strict';
+console.log("Card loaded");
+const buttons = document.querySelectorAll('[data-qa="hover"]');
+
+buttons.forEach((button) => {
+  button.addEventListener('click', (e) => {
+    e.preventDefault();
+  });
+});
