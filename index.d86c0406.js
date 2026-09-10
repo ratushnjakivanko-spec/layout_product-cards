8a936d3be07a2791a75f8ba90b369eeb58691191
Ivan Ratushnjak <ratushnjak.ivanko@gmail.com>
2026-09-10 11:51:49 +0300

    make build

---
+++ index.d86c0406.js
@@ -0,0 +1,2 @@
+console.log("Card loaded"),document.querySelectorAll('[data-qa="hover"]').forEach(e=>{e.addEventListener("click",e=>{e.preventDefault()})});
+//# sourceMappingURL=index.d86c0406.js.map
